ff49f9a9b7a91d050824ec55b1edabdfe6f39c6d
andy-engineer009 <singhmak305@gmail.com>
2025-11-29 18:38:30 +0530

    try to fix cors erros

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  // Enable CORS
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://blogvoom-backend.onrender.com',
+      // Add other allowed origins as needed
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+  
+  const port = 3000;
+  await app.listen(port);
 }
 bootstrap();
