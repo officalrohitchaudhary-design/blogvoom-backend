f78e25f54579ebac6b6b4242be53914b6228ff32
andy-engineer009 <singhmak305@gmail.com>
2025-11-29 22:26:33 +0530

    new code push

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,12 @@ async function bootstrap() {
   
   // Enable CORS
   app.enableCors({
-    origin: ['*'],
+    origin: ['https://www.blogvoom.in/', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type'],
-    credentials: false,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   });
   
-  const port = 3000;
-  await app.listen(port);
+  await app.listen(3000);
 }
 bootstrap();
